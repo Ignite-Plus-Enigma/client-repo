4011bed794b3899a913ccf8f13cf412995759d65
unknown <ankithac123@gmail.com>
2020-07-29 22:43:19 +0530

    updated to suit the UI

--- service/DigitalLibrary/src/main/java/com/trust/samarthanam/DigitalLibrary/Service/CategoryService.java
+++ service/DigitalLibrary/src/main/java/com/trust/samarthanam/DigitalLibrary/Service/CategoryService.java
@@ -1,8 +1,6 @@
 package com.trust.samarthanam.DigitalLibrary.Service;
 
-import com.trust.samarthanam.DigitalLibrary.Model.Books;
 import com.trust.samarthanam.DigitalLibrary.Model.Category;
-import com.trust.samarthanam.DigitalLibrary.dao.BooksRepo;
 import com.trust.samarthanam.DigitalLibrary.dao.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,7 +20,6 @@ public class CategoryService {
         return categoryRepo.findAll();
     }
 
-
 //----------------------------------------list all categories ----------------------------------------------------------
     public ArrayList<String> listCategory() {
         ArrayList<String> categories=new ArrayList<String>();
@@ -34,17 +31,15 @@ public class CategoryService {
     }
 
 
-//---------------------------------------list all subcategories of a main category--------------------------------------
+////---------------------------------------list all subcategories of a main category--------------------------------------
     public List<String> listSubCategory(String key) {
-        ArrayList<String> subCategories=new ArrayList<String>();
         List<Category> l = categoryRepo.findAll();
-
-
-        for(Category category: l ){
-            if(category.getCategory().equals(key)){
-                subCategories.add(category.getSubCategory());
+        for(Category c : l){
+            if(c.getCategory().equals(key)){
+                return c.getSubCategory();
             }
         }
-        return subCategories;
+
+        return null;
     }
 }
