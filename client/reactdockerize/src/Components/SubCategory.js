97b8d7bd46acd2cb1140c9b7bed33b7e7fa958af
Ruchika <jain.ruchika56@gmail.com>
2020-07-30 21:22:24 +0530

    Fixed footer

--- client/reactdockerize/src/Components/SubCategory.js
+++ client/reactdockerize/src/Components/SubCategory.js
@@ -1,4 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import MediaCard from "./home/Card";
+import ButtonBase from '@material-ui/core/ButtonBase';
+import {NavLink} from 'react-router-dom';
 import axios from "axios"
 
 class SubCategory extends React.Component{
@@ -26,6 +29,27 @@ class SubCategory extends React.Component{
         
     }
 
+    // handleClick = ((book) => {
+    //     console.log(book)
+    //     setBookId(book.book.id)
+    //     console.log(book.book.format)
+    //      if(book.book.format === "PDF"){
+    //        console.log("RENDER PDF BOOK")
+    //      }
+    //      else if(book.book.format ==="Audio"){
+    //       return (
+    //         <div>
+    //            <BrowserRouter>
+    //          <Switch>
+    //            <Route exact path="Audio/" component={Audio}/>
+    //          </Switch>
+    //        </BrowserRouter>
+    //         </div>
+    //       )
+     
+           
+    //      }
+    //    });
     
     render(){
         
@@ -35,7 +59,18 @@ class SubCategory extends React.Component{
         
         return(
             
-          <h1>Hii</h1>
+          <div>
+              {this.state.books.map((book)=>(
+          
+          <div class="column">
+          <ButtonBase
+          onClick={() => handleClick({book})} >
+        <MediaCard image={book.bookImage} title={book.name} author={book.author} />
+        <NavLink exact activeClassName="current" to={`/Audio/${book.id}/`} > view</NavLink> 
+        </ButtonBase>
+      </div>
+      ))}
+          </div>
 
             
         )
