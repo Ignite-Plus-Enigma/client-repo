d0e9f69914455209fc7730cb1de8dac749e71c68
Prakruthi <prakruthim25@gmail.com>
2020-08-06 00:31:54 +0530

    rotating carousel added

--- client/reactdockerize/src/Components/HomePage/Browse.js
+++ client/reactdockerize/src/Components/HomePage/Browse.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Row from "./Row.js";
 import './styles.css';
+import AutoRotatingCarousel from './AutoRotatingCarousel'
 
 export default function Browse() {
     
   return (
     
     <div>
+      <AutoRotatingCarousel/>
       <Row name="Recently Added" forapicall="recentlyadded"   />
       <Row
         name="Most Viewed" forapicall="most-viewed"
