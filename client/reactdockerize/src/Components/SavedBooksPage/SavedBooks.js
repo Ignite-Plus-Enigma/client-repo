bd251fcd194984728f09d6017f9deff8ebd767f1
unknown <ankithac123@gmail.com>
2020-08-12 10:07:36 +0530

    saved books grid integrated with backend

--- client/reactdockerize/src/Components/SavedBooksPage/SavedBooks.js
+++ client/reactdockerize/src/Components/SavedBooksPage/SavedBooks.js
@@ -52,151 +52,39 @@ const useStyles = makeStyles((theme) => ({
 
 function SavedBooks(props){
       const msg = useContext(userContext)
-  console.log("Google id "+msg);
-    const [open, setOpen] = React.useState(false);
-    const [id,setId] = useState(" ")
-    const handleClickOpen = () => {
-        if(state == "Sign In"){
-            setOpen(true);
-        }
-        else if(state == "Sign Out"){
-            setState("Sign In")
-        }
-      
-    };
-    
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-    const [flag,setFlag] = useState(false)
-    const [name,setName] = useState("")
-    const [state, setState] = useState("Sign In")
- 
-
-    function responseGoogle(response){
-        console.log(response.profileObj)
-        setState("Sign Out")
-        setName(response.profileObj.name)
-        setOpen(false)
-        const email = response.profileObj.email;
-        setId(email)
-        console.log(email)
-        console.log(id)
-    }
-
-    function failedLogin(response){
-        setState("Sign In")
-    }
+      // alert("Google id "+msg);
     
     const classes = useStyles();
-    const [books,setBooks] = useState([{
-      "id": "1",
-      "name": "Alice's Adventures in Wonderland",
-      "isbn": "935-678-234-23",
-      "author": "Lewis Carrol",
-      "format": {
-          "pdf": {
-              "url": "https://drive.google.com/uc?export=view&id=1K7eJ9Wvlvo8_2BpMmqshQi8OKBijl49E"
-          },
-          "audio": {
-              "url": "https://drive.google.com/uc?export=view&id=1m7SqrxcbAxKyDUfYzcKP9Rbn1EMJl9QH"
-          }
-      },
-      "language": " English",
-      "bookImage": "https://drive.google.com/uc?export=view&id=1Sqc63-nx8nPnBGkOgpqs1uFLr1JcW60R",
-      "description": "Alice's Adventures in Wonderland, widely beloved British children's book by Lewis Carroll.The story centres on Alice, a young girl who falls asleep in a meadow and dreams that she follows the White Rabbit down a rabbit hole",
-      "views": 4,
-      "category": "Children",
-      "subCategory": [
-          "Story books"
-      ],
-      "rating": 5
-  },
-  {
-      "id": " 2",
-      "name": "A Child's Garden of Verses",
-      "isbn": "983-673-232-43",
-      "author": "Robert Louis Stevenson",
-      "format": {
-          "pdf": {
-              "url": "https://drive.google.com/uc?export=view&id=1liGWXNRlO0srUC6AQQtcKhepDzHJuVW3"
-          },
-          "audio": {
-              "url": "https://drive.google.com/uc?export=view&id=1J1LhIsALHlKK9kP3qPcWjl_-yEO1V3MB"
-          }
-      },
-      "language": " English",
-      "bookImage": "https://drive.google.com/uc?export=view&id=1uB585wpdMw7tq950nI6sD4QptPLEapRL",
-      "description": "",
-      "views": 2,
-      "category": "Children",
-      "subCategory": [
-          "Poems"
-      ],
-      "rating": 3
-  }])
+    const [books,setBooks] = useState([])
     const history = useHistory();
-    // const [mainCategoryProps, setMainCategoryProps] = useState("Textbooks")
+
     function handleAudio(book){
       console.log(book.book.id)
-      // eslint-disable-next-line no-restricted-globals
       history.push(`/Audio/${book.book.id}/`)
     }
     function handlePdf(book){
-      console.log(book.book.id)
-      // eslint-disable-next-line no-restricted-globals
       history.push(`/PDF/${book.book.id}/`)
     }
 
     const fetchData = () => {
-        console.log(props.location.pathname)
-        console.log(props)
-        const v= props.location.pathname.split("/")[2]
-        const uniqueId = props.location.pathname.split("/")[3]
-        console.log(uniqueId)
-        // const userSavedBooksApiEndPoint = 'http://localhost:8050/api/v1/books/subcategory/'+uniqueId
-        const userSavedBooksApiEndPoint = "static/recentlyAddedHome.json"
-        // axios.get(userSavedBooksApiEndPoint)
-        // .then(response => response.data)
-        // .then((data) => {
-        //     console.log(data);
-        //     setBooks(data)
-        // })
+        // console.log(props.location.pathname)
+        // console.log(props)
+        // const v= props.location.pathname.split("/")[2]
+        // const uniqueId = props.location.pathname.split("/")[3]
+        // console.log(uniqueId)
+        const userSavedBooksApiEndPoint = 'http://localhost:8050/api/v1//user/104189239213398936383/savedbooks'
+        // const userSavedBooksApiEndPoint = "static/recentlyAddedHome.json"
+        axios.get(userSavedBooksApiEndPoint)
+        .then(response => response.data)
+        .then((data) => {
+            console.log(data);
+            setBooks(data)
+        })
     }
     useEffect(() => {
         fetchData()
     }, [])
-    if(state == "Sign In"){
-        return(
-            <div>
-             <Button  color="secondary" onClick={handleClickOpen}>
-        {state}
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Have a Google Account?"}
-        </DialogTitle>
-        <DialogActions>
-          <GoogleLogin
-            clientId="992798065124-l39cdadgtpb6l4ikt8nf4m909vspjnr0.apps.googleusercontent.com"
-            buttonText="Sign In With Google"
-            onSuccess={responseGoogle}
-            onFailure={failedLogin}
-            cookiePolicy={"single_host_origin"}
-          />
-        </DialogActions>
-      </Dialog>
 
-            </div>
-        )
-    }
-    else{
         return(
           <div>
         
@@ -257,14 +145,14 @@ function SavedBooks(props){
                
                <Grid item xs={1}>
                    <div>
-                <CheckCircleIcon fontSize="medium" ></CheckCircleIcon>
+                <CheckCircleIcon fontSize="medium"  onClick={() => handleAudio({book})}></CheckCircleIcon>
                 {/* <Typography variant="subtitle2">Save</Typography> */}
                 </div>
               </Grid>
               
                <Grid item xs={1}>
                    <div>
-                <BookmarkIcon fontSize="medium" ></BookmarkIcon>
+                <BookmarkIcon fontSize="medium"  onClick={() => handleAudio({book})} ></BookmarkIcon>
                 {/* <Typography variant="subtitle2">Save</Typography> */}
                 </div>
               </Grid>
@@ -284,7 +172,54 @@ function SavedBooks(props){
         )
            
         
-    }
+    
     }
 
 export default SavedBooks;
+
+// {
+//   "id": "1",
+//   "name": "Alice's Adventures in Wonderland",
+//   "isbn": "935-678-234-23",
+//   "author": "Lewis Carrol",
+//   "format": {
+//       "pdf": {
+//           "url": "https://drive.google.com/uc?export=view&id=1K7eJ9Wvlvo8_2BpMmqshQi8OKBijl49E"
+//       },
+//       "audio": {
+//           "url": "https://drive.google.com/uc?export=view&id=1m7SqrxcbAxKyDUfYzcKP9Rbn1EMJl9QH"
+//       }
+//   },
+//   "language": " English",
+//   "bookImage": "https://drive.google.com/uc?export=view&id=1Sqc63-nx8nPnBGkOgpqs1uFLr1JcW60R",
+//   "description": "Alice's Adventures in Wonderland, widely beloved British children's book by Lewis Carroll.The story centres on Alice, a young girl who falls asleep in a meadow and dreams that she follows the White Rabbit down a rabbit hole",
+//   "views": 4,
+//   "category": "Children",
+//   "subCategory": [
+//       "Story books"
+//   ],
+//   "rating": 5
+// },
+// {
+//   "id": " 2",
+//   "name": "A Child's Garden of Verses",
+//   "isbn": "983-673-232-43",
+//   "author": "Robert Louis Stevenson",
+//   "format": {
+//       "pdf": {
+//           "url": "https://drive.google.com/uc?export=view&id=1liGWXNRlO0srUC6AQQtcKhepDzHJuVW3"
+//       },
+//       "audio": {
+//           "url": "https://drive.google.com/uc?export=view&id=1J1LhIsALHlKK9kP3qPcWjl_-yEO1V3MB"
+//       }
+//   },
+//   "language": " English",
+//   "bookImage": "https://drive.google.com/uc?export=view&id=1uB585wpdMw7tq950nI6sD4QptPLEapRL",
+//   "description": "",
+//   "views": 2,
+//   "category": "Children",
+//   "subCategory": [
+//       "Poems"
+//   ],
+//   "rating": 3
+// }
